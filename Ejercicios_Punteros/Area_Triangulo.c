c7a9df8cb579f9866f607811e94d0d1a774b279c
Mathías D. Castillo <mdcy1123@gmail.com>
2025-05-14 10:08:14 -0500

    Ejercicios Punteros

---
+++ Ejercicios_Punteros/Area_Triangulo.c
@@ -0,0 +1,25 @@
+/*de un triangulo que esta en un plano cartesiano, se debe obtener el área*/
+
+#include <stdio.h>
+#include <math.h>
+#include "FUnciones_Area.h"
+
+int main (int argc, char *argv[]) {
+
+    float puntos[3][2]; // matriz de 3x2
+    float l1, l2, l3; // lados del triangulo
+    float s, p; // semiperimetro y perimetro
+    float ar; // area del triangulo
+
+    leerComponentes(puntos);
+
+    calcularDistancia(&l1, &l2, &l3, puntos);
+
+    perimetro(&s, &p, l1, l2, l3);
+
+    ar = area(s, l1, l2, l3);
+
+    printf("El perimetro del triangulo es: %.2f\n", p);
+    printf("El area del triangulo es: %.2f\n", ar);
+    return 0;
+}
